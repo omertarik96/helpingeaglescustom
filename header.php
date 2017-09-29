7562d1696968242f7589de92fae3a72f26e763a0
Jordan Carswell <18094493+jordancarswell@users.noreply.github.com>
2017-09-29 17:46:47 -0500

    Update header.php

--- header.php
+++ header.php
@@ -76,7 +76,7 @@
                     	$custom_logo_id = get_theme_mod( 'custom_logo' );
 			$logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
 			if ( has_custom_logo() ) {
-			        echo '<img alt=""'. bloginfo('name');.'" src="'. esc_url( $logo[0] ) .'">';
+			        echo '<img src="'. esc_url( $logo[0] ) .'">';
 			} else {
 			        echo '<h1>'. get_bloginfo( 'name' ) .'</h1>';
 			} ?>
