b8a837f537947d14a84c7eaddaeda1c8e782d0bb
Jordan Carswell <18094493+jordancarswell@users.noreply.github.com>
2017-10-01 15:35:45 -0500

    Update header.php

--- header.php
+++ header.php
@@ -8,6 +8,11 @@
 	    	global $page, $paged;
 	    	wp_title( '|', true, 'right' );
 	    	bloginfo( 'name' );
+	    	$site_description = get_bloginfo( 'description', 'display' );
+	    	if ( $site_description && ( is_home() || is_front_page() ) )
+		    echo " | $site_description";
+	    	if ( $paged >= 2 || $page >= 2 )
+		    	echo ' | ' . sprintf( __( 'Page %s', 'twentyeleven' ), max( $paged, $page ) );
 	    	?>
 		</title>
 
