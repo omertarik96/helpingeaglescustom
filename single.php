0edd9bc8c36591d31b692580b407f45b87f647ab
Jordan Carswell <18094493+jordancarswell@users.noreply.github.com>
2017-10-10 17:29:15 -0500

    Fixed added period in content area

--- single.php
+++ single.php
@@ -30,7 +30,7 @@
 
                            <!--TITLE AND CONTENT-->
                            <h3><?php the_title(); ?></h3>
-                            <p><?php the_content(); ?>.</p>
+                            <?php the_content(); ?>
 
                         <!--RESPOND SECTION-->
                         <div class="respond">
